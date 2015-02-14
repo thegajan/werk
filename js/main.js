c690a09a2f41923cbb100ecb703c54a7551a402e
Gajan Nagaraj <thegajannagaraj@gmail.com>
2015-02-14 00:10:34 -0800

    Made sidebar

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
 $(document).ready(function () {
-//    $('.add').mouseenter(function(){
-//    $('.add').css('rotate','360deg').animate({duration: 5000});
-//    })
 
 });
--- main.php
+++ main.php
@@ -14,7 +14,7 @@ include_once "header.php";
         <tr id="home" class="hoem"><td><div><img src="img_fonts/home.png" class="side-bar-icon" id="home-icon"><h2>Home</h2></div></td></tr>
         <tr id="stats"><td><div><img src="img_fonts/calculator.png" class="side-bar-icon" id="calc-icon"><h2>Stats</h2></div></td></tr>
         <tr id="graphs"><td><div><div data-icon="ei-chart" class="side-bar-icon" id="graph-icon"></div><h2>Graphs</h2></div></td></tr>
-        <tr id="settings"><td><div><div data-icon="ei-gear" class="side-bar-icon" id="gear-icon"></div><h2>Settings</h2></div></td></tr>
+        <tr id="settings    "><td><div><div data-icon="ei-gear" class="side-bar-icon" id="gear-icon"></div><h2>Settings</h2></div></td></tr>
     <tr class="space"><td></td></tr>
     </tbody>
 </table>
