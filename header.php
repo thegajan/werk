dfe1a79049a4c2f3e5ee7f8d2a690f431930c0e0
Gajan Nagaraj <thegajannagaraj@gmail.com>
2015-02-14 02:02:20 -0800

    Added new font and made add task

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
     <!--    <link rel="shortcut icon" href="img_fontss/favicon.jpg" type="image/x-icon">-->
     <title>A Time Productivity App</title>
     <script type="text/javascript" src="js/jquery-1.11.1.js"></script>
+    <script type="text/javascript" src="js/jquery-ui.js"></script>
     <script type="text/javascript" src="js/main.js"></script>
     <script type="text/javascript" src="js/evil-icons.min.js"></script>
     <link rel="stylesheet" href="css/evil-icons.min.css">
@@ -38,18 +39,45 @@
                 <input type="text" id="task-name" class="input section" name="task-name" placeholder="Task Name">
                 <textarea class="textarea seciton" id="taskDescription" rows="5" name="taskDescription"
                           placeholder="Task Description"></textarea>
+
+                <h1>Task Timings</h1>
+
+                <h2>Start Time:</h2> <input type="text" id="start-date" class="time datepicker" name="startDay"
+                                            placeholder="Start Day"><br></br.>
+                <input type="text" id="startHour" name="startHour" placeholder="Hour"
+                       class="time start times">:<input type="text" id="startMinute" name="min" placeholder="Minute"
+                                                        class="time start times">:<select name="start-time"
+                                                                                          id="startTOD">
+                    <option></option>
+                    <option>AM</option>
+                    <option>PM</option>
+                </select>
+
+                <h2>End Time:</h2> <input type="text" id="end-date" class="time datepicker" name="endDay"
+                                          placeholder="Ens Day"><br></br.>
+                <input type="text" id="endHour" name="endHour" placeholder="Hour"
+                       class="time end times">:<input type="text" id="endMinute" name="min" placeholder="Minute"
+                                                      class="time end times">:<select name="end-time" id="endTOD">
+                    <option></option>
+                    <option>AM</option>
+                    <option>PM</option>
+                </select>
+                <br>
+                <ul id="burst-div">
+                    <li><input type="checkbox" id="burst" name="burst" class="input section"></li>
+                    <li id="burst-writing">Do you want burst notifications to be enabled?</li>
+                </ul>
             </div>
-        </div>
-        <div class="ico-shit other">
-            <div data-icon="ei-bell"></div>
-        </div>
-        <div class="ico-shit stuff">
-            <div data-icon="ei-user"></div>
-        </div>
-        <!--    <ul>-->
-        <!--    <li class="nav-li ico-font" id="add-task"><div data-icon="ei-plus"></div></li>-->
-        <!--    <li class="nav-li ico-font" id="notifications"><div data-icon="ei-bell"></div></li>-->
-        <!--    <li class="nav-li" id="user-icon"><div id="user-overlay"><div data-icon="ei-user" id="user-icon-stuff"></div></li>-->
-        <!--    </ul>-->
+            <div class="ico-shit other">
+                <div data-icon="ei-bell"></div>
+            </div>
+            <div class="ico-shit stuff">
+                <div data-icon="ei-user"></div>
+            </div>
+            <!--    <ul>-->
+            <!--    <li class="nav-li ico-font" id="add-task"><div data-icon="ei-plus"></div></li>-->
+            <!--    <li class="nav-li ico-font" id="notifications"><div data-icon="ei-bell"></div></li>-->
+            <!--    <li class="nav-li" id="user-icon"><div id="user-overlay"><div data-icon="ei-user" id="user-icon-stuff"></div></li>-->
+            <!--    </ul>-->
     </nav>
 </header>
--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
-
+//    $(function () {
+//        $(".datepicker").datepicker({
+//            inline: true,
+//            "changeMonth":true,
+//            "changeYear":true,
+//            showOtherMonths: true,
+//            dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+//        });
+//    });
+//    $(".startDate_picker").datepicker({
+////            "dateFormat":"dd/MM/y",
+//        "changeMonth":true,
+//        "changeYear":true
+//    });
 });
