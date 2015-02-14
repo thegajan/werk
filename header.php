7e38a16fee6e2313df43736586d015dd097c928a
Gajan Nagaraj <thegajannagaraj@gmail.com>
2015-02-14 01:28:17 -0800

    Added new font and made add task

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,400,300,600' rel='stylesheet'
           type='text/css'>
     <meta charset="utf-8"/>
-<!--    <link rel="shortcut icon" href="img_fontss/favicon.jpg" type="image/x-icon">-->
+    <!--    <link rel="shortcut icon" href="img_fontss/favicon.jpg" type="image/x-icon">-->
     <title>A Time Productivity App</title>
     <script type="text/javascript" src="js/jquery-1.11.1.js"></script>
     <script type="text/javascript" src="js/main.js"></script>
@@ -21,7 +21,9 @@
 
         <div id="logo">
             <ul>
-                <li onclick="document.location='main.php'"><div data-icon="ei-pencil"></div></li>
+                <li onclick="document.location='main.php'">
+                    <div data-icon="ei-pencil"></div>
+                </li>
                 </li>
                 <!--				<li style="cursor:pointer;" id="menuIcon"><img alt="menuBTN" src="images/MenuButton.png"></li>-->
                 <li onclick="document.location='main.php'" style="cursor: pointer;">Werk</li>
@@ -29,13 +31,25 @@
         </div>
     </div>
     <nav>
-        <div class="ico-shit other add"><div data-icon="ei-plus"></div></div>
-        <div class="ico-shit other"><div data-icon="ei-bell"></div></div>
-        <div class="ico-shit stuff"><div data-icon="ei-user"></div></div>
-<!--    <ul>-->
-<!--    <li class="nav-li ico-font" id="add-task"><div data-icon="ei-plus"></div></li>-->
-<!--    <li class="nav-li ico-font" id="notifications"><div data-icon="ei-bell"></div></li>-->
-<!--    <li class="nav-li" id="user-icon"><div id="user-overlay"><div data-icon="ei-user" id="user-icon-stuff"></div></li>-->
-<!--    </ul>-->
+        <div class="ico-shit other add">
+            <div data-icon="ei-plus"></div>
+            <div class="drop-down bubble" id="add-drop-down">
+                <h1>Add a Task</h1>
+                <input type="text" id="task-name" class="input section" name="task-name" placeholder="Task Name">
+                <textarea class="textarea seciton" id="taskDescription" rows="5" name="taskDescription"
+                          placeholder="Task Description"></textarea>
+            </div>
+        </div>
+        <div class="ico-shit other">
+            <div data-icon="ei-bell"></div>
+        </div>
+        <div class="ico-shit stuff">
+            <div data-icon="ei-user"></div>
+        </div>
+        <!--    <ul>-->
+        <!--    <li class="nav-li ico-font" id="add-task"><div data-icon="ei-plus"></div></li>-->
+        <!--    <li class="nav-li ico-font" id="notifications"><div data-icon="ei-bell"></div></li>-->
+        <!--    <li class="nav-li" id="user-icon"><div id="user-overlay"><div data-icon="ei-user" id="user-icon-stuff"></div></li>-->
+        <!--    </ul>-->
     </nav>
 </header>
