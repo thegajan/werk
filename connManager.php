af88de838298b1922c8677e62868e9ffb2299fe9
Gajan Nagaraj <thegajannagaraj@gmail.com>
2015-02-14 00:17:45 -0800

    Adding Db shit

---
+++ connManager.php
@@ -0,0 +1,15 @@
+<?php
+class connManager{
+    function GetConnection(){
+        $dbhost = 'localhost';
+        $dbuser = 'adminC7Gaxb2';
+        $dbpass = 'U3crin9J3GXq';
+        $dbName = "werk";
+        $conn = mysql_connect($dbhost, $dbuser, $dbpass);
+        mysql_select_db($dbName) or die('Could not select database' . mysql_error ());
+        return $conn;
+    }
+}
+
+
+?>
