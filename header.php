28ef292061ead87b300cbe25326a2cf1d2c4c43f
Gajan Nagaraj <thegajannagaraj@gmail.com>
2015-02-13 19:23:05 -0800

    First Change

---
+++ header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<head>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,400,300,600' rel='stylesheet'
+          type='text/css'>
+    <meta charset="utf-8"/>
+<!--    <link rel="shortcut icon" href="img_fontss/favicon.jpg" type="image/x-icon">-->
+    <title>A Time Productivity App</title>
+    <script type="text/javascript" src="js/jquery-1.11.1.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+    <script type="text/javascript" src="js/evil-icons.min.js"></script>
+    <link rel="stylesheet" href="css/evil-icons.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <meta name="description"
+          content="A Time Productivity App.">
+    <meta name="keywords"
+          content="WERK, time management, productivity, Gajan  Nagaraj, Alex Erf, werk">
+    <meta name="author" content="Gajan Nagaraj and Alex Erf">
+</head>
+<header>
+    <div id="whole-logo">
+
+        <div id="logo">
+            <ul>
+                <li><div data-icon="ei-navicon"></div></li>
+                </li>
+                <!--				<li style="cursor:pointer;" id="menuIcon"><img alt="menuBTN" src="images/MenuButton.png"></li>-->
+                <li onclick="document.location='main.php'" style="cursor: pointer;">Werk</li>
+            </ul>
+        </div>
+    </div>
+    <nav>
+        <div class="ico-shit other add"><div data-icon="ei-plus"></div></div>
+        <div class="ico-shit other"><div data-icon="ei-bell"></div></div>
+        <div class="ico-shit stuff"><div data-icon="ei-user"></div></div>
+<!--    <ul>-->
+<!--    <li class="nav-li ico-font" id="add-task"><div data-icon="ei-plus"></div></li>-->
+<!--    <li class="nav-li ico-font" id="notifications"><div data-icon="ei-bell"></div></li>-->
+<!--    <li class="nav-li" id="user-icon"><div id="user-overlay"><div data-icon="ei-user" id="user-icon-stuff"></div></li>-->
+<!--    </ul>-->
+    </nav>
+</header>
---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+//    $('.add').mouseenter(function(){
+//    $('.add').css('rotate','360deg').animate({duration: 5000});
+//    })
+
+});
