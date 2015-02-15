eb0158baa103cedac55223c78f37eb8d94e1cd40
Gajan Nagaraj <thegajannagaraj@gmail.com>
2015-02-14 23:40:32 -0800

    need new db creds

--- connManager.php
+++ connManager.php
@@ -1,10 +1,10 @@
 <?php
 class connManager{
     function GetConnection(){
-        $dbhost = 'localhost';
-        $dbuser = 'adminC7Gaxb2';
-        $dbpass = 'U3crin9J3GXq';
-        $dbName = "werk";
+//        $dbhost = 'localhost';
+//        $dbuser = 'adminC7Gaxb2';
+//        $dbpass = 'U3crin9J3GXq';
+//        $dbName = "werk";
         $conn = mysql_connect($dbhost, $dbuser, $dbpass);
         mysql_select_db($dbName) or die('Could not select database' . mysql_error ());
         return $conn;
