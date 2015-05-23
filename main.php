c331c9532bc228b5a25290a9d7e55222f3baaeac
Gajan Nagaraj <thegajannagaraj@gmail.com>
2015-05-22 17:22:09 -0700

    fixed more stuff

--- main.php
+++ main.php
@@ -49,6 +49,7 @@ include_once "header.php";
 <section id="main-contents">
     <div id="curr-task-content">
         <div id="curr-task-header">
+            <input class="header" type=""
             <h1 class="header">Tasks</h1>
 
             <img src="img_fonts/android-more-vertical.svg" class="icons header-icons">
